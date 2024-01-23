effc5704ca4727b25370805d49598a8ead7c3cbf
nboynick <nboynick@icloud.com>
2024-01-23 12:45:11 -0500

    Assignment 2: Microcontroller - 1

---
+++ microcontroller/src/main.py
@@ -0,0 +1,14 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Creed, Augie, Nathaniel                                      #
+# 	Created:      1/23/2024, 12:39:54 PM                                       #
+# 	Description:  EXP project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
