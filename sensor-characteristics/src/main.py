a6b5fe38521feaca51f1717d6d5099e60ad045f9
nboynick <nboynick@icloud.com>
2024-01-27 16:50:58 -0500

    Assignment 3: Sensor Characteristics - 0
    Project initialization/setup.

---
+++ sensor-characteristics/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       nboynick                                                     #
+# 	Created:      1/27/2024, 4:47:56 PM                                        #
+# 	Description:  EXP project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello EXP")
+
+
+        
