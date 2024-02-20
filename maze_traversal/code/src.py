0e37421412fb39aabb47374dd1cefca2fbe59192
nboynick <76922906+nboynick@users.noreply.github.com>
2024-02-20 13:15:34 -0500

    Post lecture update
    Tuesday, February 20th, 2024.

--- maze_traversal/code/src.py
+++ maze_traversal/code/src.py
@@ -19,6 +19,7 @@ limit_switch_b = Limit(brain.three_wire_port.b)
 limit_switch_c = Limit(brain.three_wire_port.c)
 servo_a = Servo(brain.three_wire_port.a)
 distance_8 = Distance(Ports.PORT8)
+orientation_f = PotentiometerV2(brain.three_wire_port.f)
 
 
 # Wait for sensor(s) to fully initialize
@@ -48,6 +49,7 @@ def calibrate_drivetrain():
 
 # Library imports
 from vex import *
+import math
 # COPY CODE FROM src.py INTO THIS FILE
 
 
@@ -55,14 +57,37 @@ from vex import *
 stabilize_heading = True # Turn off temporarily to be able to turn a corner
 last_stabilization_time = 0
 current_heading = 0 # Either 0, 90, 180, or 270; Used to stabilize the heading
+old_distance = 0
+
+def print_debug(func_name):
+        print("T:" + str(round(brain.timer.time(SECONDS), 1)), end="")
+        print("  D:" + str(round(distance_8.object_distance(), 1)), end="")
+        print("  F:" + func_name)
+
+
+def check_heading():
+    global current_heading
+    new_heading = brain_inertial.heading(DEGREES)
+    old_temp = current_heading + 90
+    new_heading += 90
+    if new_heading < (old_temp - 5) or new_heading > (old_temp + 5):
+        return True
+    return False
+
 
 def correct_heading():
+    drivetrain.stop()
     global stabilize_heading
+    global last_stabilization_time
     global current_heading
     while True:
         # Note: It might be simpler/faster to simply write a wait(2,SEC) in each loop iteration
-        if stabilize_heading and last_stabilization_time < (brain.timer.time(SECONDS) - 2):
+        if stabilize_heading and last_stabilization_time < (brain.timer.time(SECONDS) - 0.5) and check_heading():
+            print("Check heading")
             drivetrain.turn_to_heading(current_heading, DEGREES, wait=True)
+            last_stabilization_time = brain.timer.time(SECONDS)
+            drivetrain.drive(FORWARD)
+
 
 def init():
     # Drivetrain
@@ -72,9 +97,86 @@ def init():
     heading_correction_thread(correct_heading)
     wait(15, MSEC)
     heading_correction_thread.broadcast()
+    servo_a.set_position(-50, DEGREES)
+    wait(600, MSEC)
+
+
+def turn_left(old_distance):
+    global stabilize_heading
+    stabilize_heading = False
+
+    global current_heading
+    current_heading -= 90
+    if current_heading < 0:
+        current_heading += 360
+
+    clearance = old_distance * (math.cos(math.radians(50)))
+    clearance += 12
+    drivetrain.drive_for(FORWARD, clearance, INCHES)
+
+    drivetrain.turn_to_heading(current_heading, DEGREES)
+
+    stabilize_heading = True
+
+
+def turn_right():
+    global stabilize_heading
+    stabilize_heading = False
+
+    global current_heading
+    current_heading += 90
+    if current_heading > 360:
+        current_heading -= 360
+
+    drivetrain.turn_to_heading(current_heading, DEGREES)
+
+    stabilize_heading = True
+
+
+def turn_right_bump():
+    global old_distance
+
+    print_debug("right-bump")
+
+    global stabilize_heading
+    stabilize_heading = False
+
+    drivetrain.drive_for(REVERSE, 4, INCHES)
+    turn_right()
+    drivetrain.drive(FORWARD)
+
+    old_distance = distance_8.object_distance(INCHES)
+
+    stabilize_heading = True
+
 
 def main():
     init()
-    pass
+
+    global old_distance
+
+    limit_switch_b.pressed(turn_right_bump)
+    # limit_switch_c.pressed(turn_right_bump)
+    old_distance = distance_8.object_distance(INCHES)
+    drivetrain.set_drive_velocity(30, PERCENT)
+    drivetrain.set_turn_velocity(40, PERCENT)
+    drivetrain.drive(FORWARD)
+    while True:
+        new_distance = distance_8.object_distance(INCHES)
+        print(round(distance_8.object_distance(), 1))
+        if new_distance > (old_distance + 7):
+            drivetrain.stop()
+            print_debug("left")
+            turn_left(old_distance)
+            drivetrain.drive(FORWARD)
+            old_distance = distance_8.object_distance(INCHES)
+        elif new_distance < (old_distance - 1):
+            drivetrain.stop()
+            print_debug("right")
+            turn_right()
+            drivetrain.drive(FORWARD)
+            old_distance = distance_8.object_distance(INCHES)
+        else:
+            old_distance = new_distance
 
 main()
